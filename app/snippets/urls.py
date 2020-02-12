42288282b3f71b142409cdc7cdaa19f46b707124
parkhongbeen <pack122@naver.com>
2020-02-12 13:12:44 +0900

    apis를 package화 하였음

--- app/snippets/urls.py
+++ app/snippets/urls.py
@@ -1,8 +1,13 @@
 from django.urls import path
-from . import views
+
+from .apis import api_view
 
 app_name = 'snippets'
 urlpatterns = [
-    path('snippets/', views.snippet_list),
-    path('snippets/<int:pk>/', views.snippet_detail),
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    # Class-based view를 사용하는 경우, as_view()함수를 호출
+    path('snippets/', api_view.SnippetListCreateAPIView.as_view()),
+    path('snippets/<int:pk>/', api_view.SnippetRetrieveUpdateDestroyAPIView.as_view()),
 ]
